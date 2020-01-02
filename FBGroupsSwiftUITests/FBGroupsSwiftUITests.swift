4fe383ee741ea3fd9986c577e22a626721468dda
Joel Junior <joel.asj2@gmail.com>
2020-01-02 15:03:40 -0300

    Initial Commit

---
+++ FBGroupsSwiftUITests/FBGroupsSwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  FBGroupsSwiftUITests.swift
+//  FBGroupsSwiftUITests
+//
+//  Created by Joel Júnior on 02/01/20.
+//  Copyright © 2020 jnr. All rights reserved.
+//
+
+import XCTest
+@testable import FBGroupsSwiftUI
+
+class FBGroupsSwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
